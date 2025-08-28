8e259cd4e8e5b90bb7ad2f70149d8ff104ab8211
Arman <mohammadjahedulislamarman@gmail.com>
2025-08-28 15:02:28 +0600

    copy button function initialize. and copy the hotline number

--- js/home.js
+++ js/home.js
@@ -7,3 +7,29 @@ function heart() {
     const heartNewValue = heartValueNumber + 1;
     heartIcon.innerText = heartNewValue;
 }
+
+// copy
+
+function copy() {
+    const hotline = document.getElementById("copy-button")
+    const hotlineValue = hotline.innerText
+    const hotlineNumber = parseInt(hotlineValue)
+
+    const hotlineNewNumber = hotlineNumber + 1;
+    hotline.innerText = hotlineNewNumber
+
+    const nationalNum = document.getElementById("national-num")
+    const nationalNumValue = nationalNum.innerText
+    const nationalNumber = parseInt(nationalNumValue)
+
+    navigator.clipboard.writeText(nationalNumber)
+    alert("নম্বর কপি হয়েছে : " + nationalNumber)
+}
+
+
+// national emergency
+document.getElementById("national-button").addEventListener("click", function (e) {
+    e.preventDefault
+
+
+})
